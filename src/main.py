fbe036ef867b65a70885a1fbe13e2ea6d28c2c47
Abel Boldú <abel.boldu@gmx.com>
2017-01-13 12:56:09 +0100

    Ansiblize

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root():
+    return app.send_static_file('result.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
